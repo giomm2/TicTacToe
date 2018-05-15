8a1bf2e874a85519bceaab6d160dbd033274c3ef
giovanny <giomm269@gmail.com>
2018-05-15 13:07:15 -0600

    Fix some code from all clases

--- detection.rb
+++ detection.rb
@@ -1,6 +1,6 @@
-class Winner
+class Detection
 
-	def check_horizontal(tic_tac_toe)
+	def self.check_horizontal(tic_tac_toe)
 		if tic_tac_toe[0] == tic_tac_toe[1] && tic_tac_toe[1] == tic_tac_toe[2]
 			return true 
 		elsif tic_tac_toe[3] == tic_tac_toe[4] && tic_tac_toe[4] == tic_tac_toe[5]
@@ -12,7 +12,7 @@ class Winner
 		end
 	end
 
-	def check_vertical(tic_tac_toe)
+	def self.check_vertical(tic_tac_toe)
 		if tic_tac_toe[0] == tic_tac_toe[3] && tic_tac_toe[3] == tic_tac_toe[6]
 			return true 
 		elsif tic_tac_toe[1] == tic_tac_toe[4]  && tic_tac_toe[4] == tic_tac_toe[7] 
@@ -24,25 +24,23 @@ class Winner
 		end
 	end
 
-	def check_cross(tic_tac_toe)
+	def self.check_cross(tic_tac_toe)
 		if tic_tac_toe[6] == tic_tac_toe[4] && tic_tac_toe[4] == tic_tac_toe[2]
 			return true 
 		elsif tic_tac_toe[0] == tic_tac_toe[4]  && tic_tac_toe[4] == tic_tac_toe[8]
-
 			return true
 		else
 			return false
 		end
 	end
 
-	def check_all(tic_tac_toe, name)
-			if check_horizontal(tic_tac_toe) || check_vertical(tic_tac_toe) || check_cross(tic_tac_toe)
-				print "#{ name } you won \n"
-				return true
-			elsif tic_tac_toe.uniq.length == 2
-				puts "Draw!!"
-				return true 
+	def self.check_all(tic_tac_toe, name)
+		if check_horizontal(tic_tac_toe) || check_vertical(tic_tac_toe) || check_cross(tic_tac_toe)
+			print "#{ name } you won \n"
+			return true
+		elsif tic_tac_toe.uniq.length == 2
+			puts "Draw!!"
+			return true 
 			end
 	end
-
 end
--- player.rb
+++ player.rb
@@ -21,4 +21,15 @@ class Player
 			end
 		end
 	end
+
+	def self.computer_turn(tic_tac_toe)
+		flag = false
+		while flag == false do
+			number = rand(0...9)
+			if tic_tac_toe[number] == number.to_s
+				flag = true
+				return number
+			end
+		end	
+	end
 end
