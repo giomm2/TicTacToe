c3d7cae412bc11c33bfb0c882421e46845fa6600
giovanny <giomm269@gmail.com>
2018-05-14 20:49:38 -0600

    class game simplified

--- game.rb
+++ game.rb
@@ -1,6 +1,7 @@
 require_relative 'player.rb'
 require_relative 'computer.rb'
 require_relative 'winner.rb'
+	
 	def player_information
 		print 'What is your name: '
 		name = gets.chomp
@@ -9,48 +10,18 @@ require_relative 'winner.rb'
 	end
 
 	def load_tic_tac_toe
-		tic_tac_toe = Array.new
 		tic_tac_toe = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
 		return tic_tac_toe
 	end
 
-	def start_game
-		computer = Computer.new
-		player = Player.new
-		detection = Winner.new
+	def start_game	
 		tic_tac_toe = load_tic_tac_toe
-		player_name = player_information
-		print "Hello #{player_name} is time to start a new tic tac toe game. \n"
 		result = false
+		player_name = player_information
 		display_board(tic_tac_toe)
 		while result == false do 
-			
-			player_number = player.player_turn(player_name,tic_tac_toe)
-			tic_tac_toe[player_number.to_i] = "X"
-			display_board(tic_tac_toe)
-			if detection.check_horizontal(tic_tac_toe, "X") || detection.check_vertical(tic_tac_toe, "X") || detection.check_cross(tic_tac_toe, "X")
-				print "#{ player_name } you won \n"
-				print "Try again /yes"
-				if gets.chomp == "yes"
-					start_game
-				else
-					print "Thanks for playing"
-				end
-			end
-
-			print "Computer turn"
-			random = computer.computer_turn(tic_tac_toe)
-			tic_tac_toe[random] = "O"
-			display_board(tic_tac_toe)
-			if detection.check_horizontal(tic_tac_toe, "O") || detection.check_vertical(tic_tac_toe, "O") || detection.check_cross(tic_tac_toe, "O")
-				print "Computer won \n"
-				print "try again /yes"
-				if gets.chomp == "yes"
-					start_game
-				else
-					print "Thanks for playing"
-				end
-			end
+			player_turn(tic_tac_toe, player_name)
+			computer_turn(tic_tac_toe)
 		end
 	end
 
@@ -61,6 +32,42 @@ require_relative 'winner.rb'
 		puts "#{tic_tac_toe[3]} | #{tic_tac_toe[4]} | #{tic_tac_toe[5]}"
 		puts line
 		puts "#{tic_tac_toe[6]} | #{tic_tac_toe[7]} | #{tic_tac_toe[8]}"
+	end
+
+	def player_turn(tic_tac_toe,player_name)
+		player = Player.new
+		detection = Winner.new
+
+		player_number = player.player_turn(player_name, tic_tac_toe)
+			tic_tac_toe[player_number.to_i] = "X"
+			display_board(tic_tac_toe)
+			if detection.check_all(tic_tac_toe,"#{ player_name }")
+				puts "Try again: yes"
+				if gets.chomp == "yes"
+					start_game
+				else
+					puts "Thanks for playing"
+					result = true
+				end
+			end
+	end
+
+	def computer_turn(tic_tac_toe) 
+		detection = Winner.new
+		computer = Computer.new
+		puts "Computer turn"
+			random = computer.computer_turn(tic_tac_toe)
+			tic_tac_toe[random] = "O"
+			display_board(tic_tac_toe)
+			if detection.check_all(tic_tac_toe,"Computer")
+				puts "try again: yes"
+				if gets.chomp == "yes"
+					start_game
+				else
+					print "Thanks for playing"
+					result = true
+				end
+		  end
 	end
 
 start_game
