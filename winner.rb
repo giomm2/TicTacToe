af2e9ea77af50de4088d093aecedd966c3cefb45
giovanny <giomm269@gmail.com>
2018-05-14 20:57:19 -0600

    check_all method added

--- winner.rb
+++ winner.rb
@@ -1,35 +1,48 @@
 class Winner
-	def check_horizontal(tic_tac_toe, sign)
-		if tic_tac_toe[0] == sign && tic_tac_toe[1] == sign && tic_tac_toe[2] == sign
+
+	def check_horizontal(tic_tac_toe)
+		if tic_tac_toe[0] == tic_tac_toe[1] && tic_tac_toe[1] == tic_tac_toe[2]
 			return true 
-		elsif tic_tac_toe[3] == sign && tic_tac_toe[4] == sign && tic_tac_toe[5] == sign
+		elsif tic_tac_toe[3] == tic_tac_toe[4] && tic_tac_toe[4] == tic_tac_toe[5]
 			return true
-		elsif tic_tac_toe[6] == sign && tic_tac_toe[7] == sign && tic_tac_toe[8] == sign
+		elsif tic_tac_toe[6] == tic_tac_toe[7] && tic_tac_toe[7] == tic_tac_toe[8]
 			return true
 		else
 			return false
 		end
 	end
 
-	def check_vertical(tic_tac_toe, sign)
-		if tic_tac_toe[0] == sign && tic_tac_toe[3] == sign && tic_tac_toe[6] == sign
+	def check_vertical(tic_tac_toe)
+		if tic_tac_toe[0] == tic_tac_toe[3] && tic_tac_toe[3] == tic_tac_toe[6]
 			return true 
-		elsif tic_tac_toe[1] == sign && tic_tac_toe[4] == sign && tic_tac_toe[7] == sign
+		elsif tic_tac_toe[1] == tic_tac_toe[4]  && tic_tac_toe[4] == tic_tac_toe[7] 
 			return true
-		elsif tic_tac_toe[2] == sign && tic_tac_toe[5] == sign && tic_tac_toe[8] == sign
+		elsif tic_tac_toe[2] == tic_tac_toe[5] && tic_tac_toe[5] == tic_tac_toe[8]
 			return true
 		else
 			return false
 		end
 	end
 
-	def check_cross(tic_tac_toe, sign)
-		if tic_tac_toe[6] == sign && tic_tac_toe[4] == sign && tic_tac_toe[2] == sign
+	def check_cross(tic_tac_toe)
+		if tic_tac_toe[6] == tic_tac_toe[4] && tic_tac_toe[4] == tic_tac_toe[2]
 			return true 
-		elsif tic_tac_toe[0] == sign && tic_tac_toe[4] == sign && tic_tac_toe[8] == sign
+		elsif tic_tac_toe[0] == tic_tac_toe[4]  && tic_tac_toe[4] == tic_tac_toe[8]
+
 			return true
 		else
 			return false
 		end
 	end
+
+	def check_all(tic_tac_toe, name)
+			if check_horizontal(tic_tac_toe) || check_vertical(tic_tac_toe) || check_cross(tic_tac_toe)
+				print "#{ name } you won \n"
+				return true
+			elsif tic_tac_toe.uniq.length == 2
+				puts "Draw!!"
+				return true 
+			end
+	end
+
 end
