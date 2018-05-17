931fea3878ff9d680e126b43bf8a8ac7d8751aa4
giovanny <giomm269@gmail.com>
2018-05-17 14:17:30 -0600

    fix code from player, game and player_spec

--- game.rb
+++ game.rb
@@ -2,9 +2,8 @@ require_relative 'player.rb'
 require_relative 'detection.rb'
 
 def create_players
-  number_of_players = 2
   players = Array.new
-  number_of_players.times do |i|
+  2.times do |i|
     puts "Please write the name of player #{i+1}"
     name = gets
     players << Player.new(name)
@@ -15,7 +14,7 @@ end
 def player_information
   players = create_players
   player_by_turn = Array.new
-  players.each_with_index do |player,index|
+  players.each_with_index do |player, index|
     puts "Player #{player.name} Do you want to be the computer type: 1 else 
       #{players[index + 1].name} is going to be the computer"
     if gets.to_i == 1
@@ -74,16 +73,7 @@ def player_turn(tic_tac_toe, player_name)
   tic_tac_toe[player_number.to_i] = "X"
   display_board(tic_tac_toe)
   if Detection.check_all(tic_tac_toe,"#{ player_name }")
-    puts "Try again: yes"
-    if gets.chomp == "yes"
-      start_game
-    else
-      puts "Thanks for playing"
-      exit
-      return true
-    end
-  else
-    return false
+    ask_try_again 
   end
 end
 
@@ -93,16 +83,17 @@ def computer_turn(tic_tac_toe, player_name)
   tic_tac_toe[random] = "O"
   display_board(tic_tac_toe)
   if Detection.check_all(tic_tac_toe,"#{player_name} (Computer)")
-    puts "try again: yes"
-    if gets.chomp == "yes"
-      start_game
-    else
-      print "Thanks for playing"
-      exit
-      return true
-    end
+    ask_try_again 
+  end
+end
+
+def ask_try_again
+  puts "try again: yes"
+  if gets.chomp == "yes"
+    start_game
   else
-    return false
+    puts "Thanks for playing!!"
+    exit
   end
 end
 
